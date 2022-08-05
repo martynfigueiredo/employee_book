dcd8e24f2cee78af0a91f1a6ca04d279da89d59e
martynsdk <martynsdk@gmail.com>
2022-08-05 08:35:40 -0300

    Setup Date Picker

--- lib/screen/add_employee_screen.dart
+++ lib/screen/add_employee_screen.dart
@@ -22,27 +22,80 @@ class _AddEmployeeScreenState extends State<AddEmployeeScreen> {
         centerTitle: true,
         actions: [
           IconButton(
-              onPressed: (){
+              onPressed: () {
                 //TODO
-
               },
               icon: const Icon(Icons.save))
         ],
       ),
       body: Column(
         children: [
-          CustomTextFormField(controller: _userNameController, txtLable: "User Name",),
-          const SizedBox(height: 8.0,),
-          CustomTextFormField(controller: _firstNameController, txtLable: "First Name",),
-          const SizedBox(height: 8.0,),
-          CustomTextFormField(controller: _lastNameController, txtLable: "Last Name",),
-          const SizedBox(height: 8.0,),
-          CustomTextFormField(controller: _dateOfBirthController, txtLable: "Date of Birth",),
-          const SizedBox(height: 8.0,),
+          CustomTextFormField(
+            controller: _userNameController,
+            txtLable: "User Name",
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          CustomTextFormField(
+            controller: _firstNameController,
+            txtLable: "First Name",
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          CustomTextFormField(
+            controller: _lastNameController,
+            txtLable: "Last Name",
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          TextFormField(
+            controller: _dateOfBirthController,
+            keyboardType: TextInputType.name,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), label: Text("Date of Birth")),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Date of Birth cannot be empty!";
+              }
+              return null;
+            },
+            onTap: () => pickDateOfBirth(context),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
         ],
       ),
     );
   }
+
+  Future<void> pickDateOfBirth(BuildContext context) async {
+    final initialDate = DateTime.now();
+    final newDate = await showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: DateTime(DateTime.now().year - 100),
+      lastDate: DateTime(DateTime.now().year + 1),
+      builder: (context, child) => Theme(
+        data: ThemeData().copyWith(
+            colorScheme: const ColorScheme.light(
+                primary: Colors.pink,
+                onPrimary: Colors.white,
+                onSurface: Colors.black),
+            dialogBackgroundColor: Colors.white),
+        child: child ?? const Text(""),
+      ),
+    );
+
+    if (newDate == null) {
+      return;
+    }
+
+    setState(() {
+      _dateOfBirthController.text = newDate.toIso8601String();
+    });
+  }
 }
-
-
